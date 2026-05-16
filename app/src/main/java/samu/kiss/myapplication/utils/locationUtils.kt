fd823c458273f00f6716bb9a523f0da0f926d158
JDOG <ortizg.jdavid@javeriana.edu.co>
2026-05-15 22:54:13 -0500

    [Feat]: Distance function added in location utils

--- app/src/main/java/samu/kiss/myapplication/utils/locationUtils.kt
+++ app/src/main/java/samu/kiss/myapplication/utils/locationUtils.kt
@@ -23,3 +23,14 @@ fun createLocationCallback(onLocationChange : (LocationResult)-> Unit): Location
     }
     return callback
 }
+
+fun distance(lat1 : Double, long1: Double, lat2:Double, long2:Double) : Double{
+    val latDistance = Math.toRadians(lat1 - lat2)
+    val lngDistance = Math.toRadians(long1 - long2)
+    val a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)+
+            Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+            Math.sin(lngDistance / 2) * Math.sin(lngDistance / 2)
+    val c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    val result = 6378 * c;
+    return Math.round(result*100.0)/100.0;
+}
